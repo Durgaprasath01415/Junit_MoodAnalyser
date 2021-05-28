053d7201965d51d431b59d651279d4afc47d589f
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-05-28 18:16:27 +0530

    ADDED: NULL Exception and try and catch to keep the flow not stoping

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,8 +1,11 @@
 public class MoodAnalyser {
-public String analyserMood(String message){
-    if(message.contains("This is a sad message"))
-        return "SAD";
-    else
-    return "Happy";
-}
+    public String analyserMood(String message) {
+        try {
+            if (message.contains("This is a sad message"))
+                return "sad";
+        } catch (NullPointerException ex) {
+            return "happy";
+        }
+        return "happy";
+    }
 }
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,8 +4,15 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
     public void testMoodAnalysis() {
-       MoodAnalyser moodAnalyser = new MoodAnalyser();
-       String mood = moodAnalyser.analyserMood("This is a sad message");
-       Assert.assertEquals("SAD" , mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyserMood("This is a sad message");
+        Assert.assertEquals("sad", mood);
+    }
+
+    @Test
+    public void givenMood_whenNull_shouldReturnTrue() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyserMood(null);
+        Assert.assertEquals("happy", mood);
     }
 }
